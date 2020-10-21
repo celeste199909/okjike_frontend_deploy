80b7044e1e935ef3869173ab4638205296410685
iceream <1598474045@qq.com>
2020-10-22 02:41:07 +0800

    deploy2

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
       port: 9000,
       proxy: {
         '/api': {  // http://localhost:8081/api/banner
-            target: 'http://localhost:9001',  // http://127.0.0.1:7001/banner
+            // 112.124.18.251
+            target: 'http://112.124.18.251',  // http://127.0.0.1:7001/banner
             changeOrigin: true,
             pathRewrite: {
               '^/api': ''
